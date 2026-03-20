3f97d14fca6af5455be384d7a5534f9fbb2c5ec9
agugus1 <atcejas@escuelasproa.edu.ar>
2026-03-20 16:02:10 -0300

    Update python2.py

---
+++ python2.py
@@ -0,0 +1,8 @@
+import pandas as pd
+df=pd.read_csv('studentsPerformance.csv')
+print((df['gender' , 'lunch']))
+print([['gender']]. head())
+print([['gender']]. head(12))
+print([['gender']]. tail())
+print([['gender']]. tail(12))
+
