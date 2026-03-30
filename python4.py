73623a656a07722931cb99be885f3f9a403161f6
agugus1 <atcejas@escuelasproa.edu.ar>
2026-03-30 15:25:12 -0300

    Add files via upload

---
+++ python4.py
@@ -0,0 +1,14 @@
+import pandas as pd
+df = pd.read_csv('StudentsPerformance.csv')
+
+print(df[['gender', 'math score']])
+
+print(df.shape)
+
+print(df.head(4))
+
+print(df[['writing score']])
+
+print(df.shape)
+
+print(df.tail(100))
